8e7cb1e27afedf0950581662c19426fc347d6f51
dmls <danmlsv@gmail.com>
2024-06-01 20:28:25 -0400

    Set re-usable test values as class props

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,6 +11,18 @@ load_dotenv()
 
 @pytest.mark.parametrize('mock_dbm', ['users'], indirect = True)
 class TestUser:
+    @pytest.fixture(autouse = True)
+    def setup(self):
+        self.email = 'user@example.com'
+        self.password = 'password'
+        self.expected_password_hash = self.get_password_hash(self.password).decode('utf-8')
+
+    def get_password_hash(self, password):
+        return bcrypt.hashpw(
+            password.encode('utf-8'),
+            os.environ.get('PASSWORD_SALT').encode('utf-8')
+        )
+
     def get_password_hash(self, password):
         return bcrypt.hashpw(
             password.encode('utf-8'),
@@ -19,30 +31,23 @@ class TestUser:
 
     def test_hash_password(self, mock_dbm):
         user = User(mock_dbm)
+        hashed = user.hash_password(self.password)
 
-        password = 'password'
-        expected_hash = self.get_password_hash(password)
-        hashed = user.hash_password(password)
-
-        assert hashed == expected_hash
+        assert hashed.decode('utf-8') == self.expected_password_hash
 
     def test_validate_password(self, mock_dbm, monkeypatch):
         user = User(mock_dbm)
 
-        email = 'user@example.com'
-        password = 'password'
-        expected_hash = self.get_password_hash(password).decode('utf-8')
-
         monkeypatch.setattr(user, 'get_user', (
-            lambda email: {'email': email, 'password': expected_hash}
+            lambda email: {'email': email, 'password': self.expected_password_hash}
         ))
 
         monkeypatch.setattr(user, 'hash_password', (
-            lambda pwd: expected_hash if pwd == password else 'wrong_hash'
+            lambda pwd: self.expected_password_hash if pwd == self.password else 'wrong_hash'
         ))
 
-        assert user.validate_password(email, password) is True
-        assert user.validate_password(email, 'wrong_password') is False
+        assert user.validate_password(self.email, self.password) is True
+        assert user.validate_password(self.email, 'wrong_password') is False
 
         monkeypatch.setattr(user, 'get_user', lambda email: None)
-        assert user.validate_password(email, password) is False
+        assert user.validate_password(self.email, self.password) is False
